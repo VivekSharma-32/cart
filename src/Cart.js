1d138d709996e8a436c17e148c93efcb8b41aea1
VivekSharma-32 <vivek.sharma1275@gmail.com>
2023-02-08 19:06:44 +0530

    changes

---
+++ src/Cart.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import CartItem from "./CartItem";
+class Cart extends Component {
+  render() {
+    return (
+      <div className="cart">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+    );
+  }
+}
+
+export default Cart;
